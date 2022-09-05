22d639df562b9375c9225c8f2988a868b6ecf24f
Simerus Mahesh <63352151+SimerusM@users.noreply.github.com>
2022-09-05 15:14:29 -0400

    Update ErrorBoundary.js

--- frontend/src/context/ErrorBoundary.js
+++ frontend/src/context/ErrorBoundary.js
@@ -13,7 +13,7 @@ class ErrorBoundary extends React.Component {
   
     componentDidCatch(error, errorInfo) {
       // You can also log the error to an error reporting service
-      console.log('hijd');
+      console.log(error);
     }
   
     render() {
